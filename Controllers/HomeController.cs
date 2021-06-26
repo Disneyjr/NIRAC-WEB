094e50a24b9a74fb187e54cbbb4051c99a590bfa
Disney <dineyteixeira@gmail.com>
2021-06-25 21:13:46 -0400

    Inserção dos primeiros arquivos

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace NIRAC_WEB.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
