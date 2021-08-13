0b4752e3173c6d79a7dcd46954f25c77fae568f5
Disney <dineyteixeira@gmail.com>
2021-08-13 19:38:28 -0400

    Adiciona Projeto de Teste

---
+++ NIRAC-TESTE/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NIRAC-TESTE")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NIRAC-TESTE")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("09447620-2238-4da9-b41c-89e30c03f512")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ NIRAC-TESTE/UsuarioTeste.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NIRAC_API.Controllers;
+using NIRAC_BUSINESS.Models.DAO;
+using NIRAC_BUSINESS.Models.DTO;
+namespace NIRAC_TESTE
+{
+    [TestClass]
+    public class UsuarioTeste
+    {
+        private UsuarioController usuarioController;
+        private UsuarioDAO UsuarioAntigoDAO;
+        private UsuarioDTO UsuarioAntigoDTO;
+        public UsuarioTeste()
+        {
+            this.UsuarioAntigoDAO = new UsuarioDAO();
+            this.UsuarioAntigoDTO = new UsuarioDTO();
+            this.usuarioController = new UsuarioController();
+        }
+        #region TESTES UNITÁRIOS
+        //[TestMethod]
+        //public void Post()
+        //{
+        //    UsuarioAntigoDAO.Data_Cadastro = DateTime.Now;
+        //    UsuarioAntigoDAO.Data_Update = DateTime.Now;
+        //    var response = usuarioController.Post(UsuarioAntigoDAO);
+        //    Assert.IsNotNull(UsuarioAntigoDAO.Id);
+        //    usuarioController.Delete(usuarioController._serv.DTOToDAO(response));
+        //}
+        [TestMethod]
+        public void GetAll()
+        {
+            var response = usuarioController.GetAll();
+            Assert.IsNotNull(response);
+        }
+
+        [TestMethod]
+        public void Get()
+        {
+            var response = usuarioController.Get(4);
+            Assert.AreEqual(4, response.Id);
+        }
+
+        //[TestMethod]
+        //public void Put()
+        //{
+        //    var response = usuarioController.Get(10);
+        //    response.Tipo = "Alterado";
+        //    UsuarioDTO usuarioDTO = usuarioController.Put(usuarioController._serv.DTOToDAO(response));
+        //    Assert.AreEqual("Alterado", usuarioDTO.Tipo);
+        //}
+        //[TestMethod]
+        //public void Delete()
+        //{
+        //    var response = usuarioController.Get(9);
+        //    UsuarioDTO usuarioDTO = usuarioController.Delete(usuarioController._serv.DTOToDAO(response));
+        //    Assert.AreEqual(9, usuarioDTO.Id);
+        //}
+        #endregion
+        #region TESTES FLUXOS
+        //[TestMethod]
+        //public void TesteCRUD()
+        //{
+        //    //POST
+        //    UsuarioAntigoDAO.Data_Cadastro = DateTime.Now;
+        //    UsuarioAntigoDAO.Data_Update = DateTime.Now;
+        //    var responsePOST = usuarioController.Post(UsuarioAntigoDAO);
+        //    //GETALL COM FIND NO USUARIO ADICIONADO
+        //    var responseGETALL = usuarioController.GetAll().Find(c=>c.Id == responsePOST.Id);
+        //    //GET NAQUELE USUARIO
+        //    var responseGET = usuarioController.Get(responsePOST.Id);
+        //    //UPDATE TIPO USUARIO
+        //    responseGET.Tipo = "Alterado";
+        //    UsuarioDTO usuarioDTO = usuarioController.Put(usuarioController._serv.DTOToDAO(responseGET));
+        //    //DELETE USUARIO
+        //    UsuarioDTO usuarioDTODELETE = usuarioController.Delete(usuarioController._serv.DTOToDAO(responseGET));
+        //    Assert.IsNotNull(UsuarioAntigoDAO.Id);
+        //    Assert.IsNotNull(responseGETALL);
+        //    Assert.AreEqual(responsePOST.Id, responseGET.Id);
+        //    Assert.AreEqual("Alterado", usuarioDTO.Tipo);
+        //    Assert.AreEqual(responsePOST.Id, usuarioDTO.Id);
+        //}
+        #endregion
+
+    }
+}
